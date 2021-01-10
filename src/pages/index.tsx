9d29c6cc08ccf82c5ca8b818bb24efe1791fcdcb
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2021-01-10 14:48:33 -0300

    client side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,37 @@
-import Head from 'next/head'
-import { Title } from '../styles/pages/Home'
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { Title } from "../styles/pages/Home";
+
+interface IProduct {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/recommended").then((response) =>
+      response.json().then((data) => setRecommendedProducts(data))
+    );
+  }, []);
+
   return (
     <div>
       <Title>Hello world</Title>
+
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct .title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </div>
-  )
+  );
 }
