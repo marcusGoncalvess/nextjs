6675b0a8b6ccbdc2e14000e672032528869e99b5
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2021-01-10 19:47:29 -0300

    dynamic components

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,11 +1,28 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import dymanic from "next/dynamic";
+import { useCallback, useState } from "react";
+
+const AddToCart = dymanic(() => import("../../../components/AddToCart"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+  // ssr = server-side-rendering
+});
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
+  const [addToCart, setAddToCart] = useState(false);
+
+  const handleAddToCart = useCallback(() => {
+    setAddToCart(true);
+  }, []);
 
   return (
     <div>
       <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      {addToCart && <AddToCart></AddToCart>}
     </div>
-  )
+  );
 }
